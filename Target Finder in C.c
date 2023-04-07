5b45ef97d52b011bfcf45d0983ba85baedddef27
Abdulrahman Dülek <114644911+marcusdulekilius@users.noreply.github.com>
2023-04-07 10:22:54 +0300

    Update Target Finder in C.c

--- Target Finder in C.c
+++ Target Finder in C.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-Solution of this problem in C.
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-Problem' link: https://leetcode.com/problems/two-sum/
-*/
-
 void targetfinder()
 {
     int i,j,n,t;
